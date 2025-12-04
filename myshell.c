0f67f49fb223e34405a512101c9ddbeabc4fa4e1
pruizz <lttpablo@gmail.com>
2025-12-04 13:50:11 +0100

    All steps implemented -> MiniShell completely working

--- myshell.c
+++ myshell.c
@@ -1,3 +1,4 @@
+#define _GNU_SOURCE
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -6,6 +7,7 @@
 #include <string.h>
 #include <fcntl.h>
 #include <sys/stat.h>
+#include <signal.h>
 #include "parser.h"
 
 #define MAX_LINE_BUFFER 1024
@@ -23,7 +25,7 @@ typedef struct job{
     enum job_state status;
     int job_id;
     int ncommands; //para registrar cuantos comandos tiene el pipe
-    int processes_running; //para llevar cuantos procesos quedan, cuando llegue a 0 estará a DONE
+    int pending_procs;
 } job_t;
 
 job_t job_table[MAX_JOBS]; //para guardar toda la tabla de trabajos
@@ -38,6 +40,7 @@ void execute_internal_cd(tline *line);
 void execute_internal_umask(tline *line);
 void execute_internal_exit();
 void execute_internal_jobs(tline *line);
+void execute_internal_bg(tline *line);
 //funciones auxiliares para la gestión de los jobs
 job_t *find_job(pid_t pgid); 
 void register_job(pid_t pgid, const char *command, int ncommands); 
@@ -57,6 +60,12 @@ int main(void) {
 	while (fgets(buf, MAX_LINE_BUFFER, stdin)) {
 
         check_background_jobs(); //verificamos si hay trabajos en segundo plano
+
+        // NUEVO: Ignorar señales en la shell para protegerla los hijos se encargarn de manejarlas
+        signal(SIGINT, SIG_IGN);
+        signal(SIGTSTP, SIG_IGN);
+        signal(SIGTTOU, SIG_IGN); // Evita que la shell se pare al recuperar el terminal
+        signal(SIGTTIN, SIG_IGN);
 		
 		line = tokenize(buf);
 		if (line==NULL) {
@@ -89,6 +98,10 @@ int main(void) {
             //Soy Proceso Hijo
             else if (pid == 0)
             {
+                
+                // NUEVO: Restaurar comportamiento de las señales
+                signal(SIGINT, SIG_DFL);
+                signal(SIGTSTP, SIG_DFL);
 
                 pid_t pgid_hijo = (i == 0) ? 0 : job_pgid;
                 setpgid(0, pgid_hijo); //con el set, si es (0,0) crea un grupo de procesos y para los demás pgid_hijo se unen a ese grupo
@@ -144,15 +157,41 @@ int main(void) {
                 }
             }
         }
-        if (line->background) { //se cumple si usuario pone & al final del comando
-            //en background registramos el job sin esperar
-            register_job(job_pgid, buf, line->ncommands);
-        }else{
-            //en foreground esperamos a que terminen todos los hijos
-            for(i = 0; i < line->ncommands; i++) {
-                wait(&status); 
-            }
+        
+        if (line ->background)
+        { // Proceso en segund plano background
+            register_job(job_pgid,buf,line->ncommands);
         }
+        else
+        {   //Si es foreground
+            // Cedo la terminal al grupo del hijo al estar en foreground el se encarrgar de gestionar
+            // Le digo que el grupo de procesos en primer plano es el grupo odentificado por nuestro jobpgid
+            //este sera el conjutno de comandos unidos anterioremnete un un job
+            tcsetpgrp(STDIN_FILENO, job_pgid);
+            //Apartir de ahora toda entrada estandar no se recibe en la shell como tal si no a la entrada estandr de los hijos
+            for (i = 0; i < line->ncommands; i++) {
+                //-job_pgid espera por cualquiera que pertenezca al grupo creado enmlas pipes
+                //WUNTRACED para que avise si se queda parado por Control Z 
+                //Se bloquea hasta que pase algo
+                waitpid(-job_pgid, &status, WUNTRACED);
+                
+                //Si se paro con control Z
+                if (WIFSTOPPED(status)){
+                    printf("\n[%d]+ Stopped\t%s\n", next_job_id, buf);
+                    register_job(job_pgid, buf, line->ncommands);
+                    job_t *j = find_job(job_pgid);
+                    if (j)
+                    {
+                        j->status = STOPPED;
+                    } 
+                    break; // no espero mas
+                }
+            }
+            // Recuperamos el control principal al padre
+            //Ahora una vez hayan acabado todos el control tiene que volver a ser de la shell
+            tcsetpgrp(STDIN_FILENO, getpid());
+        }
+
         printf("msh> ");			
     }
 }
@@ -238,6 +277,12 @@ int check_if_internal(tline *line)
         execute_internal_jobs(line);
         return 1;
 
+    }
+
+    if (strcmp(line -> commands[0].argv[0],"bg") == 0)
+    {
+        execute_internal_bg(line);
+        return 1;
     }
 
     return 0;
@@ -351,66 +396,102 @@ job_t *find_job(pid_t pgid){
     return NULL;
 }
 
-void check_background_jobs(){
-    pid_t pid;
+void check_background_jobs() {
+    int i;
     int status;
-    job_t *job;
-    pid_t pgid;
+    pid_t pid;
 
-    while((pid = waitpid(-1, &status, WNOHANG | WUNTRACED | WCONTINUED)) > 0){
-        //-1 para que espere el cambio de estado de cualquier proceso hijo
-        //WNOHANG para que waitpid devuelva 0 si ningún hijo ha cambiado de estado
-        //WUNTRACED para que waitpid informe si un proceso ha sido detenido
-        //WCONTINUED para que waitpid informe si un proceso ha sido reanudado
-    
-        pgid = getpgid(pid); //obtenemos el pgid del proceso que cambio de estado
+    // Recorremos la tabla de trabajos completa para verificar el estado de cada grupo activo.
+    // A diferecia de la version anterior evitamos perder referencias a ciertos jobs
+    for (i = 0; i < MAX_JOBS; i++) {
         
-        if (pgid == -1) { //control de un posible fallo
-            continue; 
-        }
+        // Solo nos interesan los trabajos que están activos y que no han terminado (DONE)
+        if (job_table[i].pgid != 0 && job_table[i].status != DONE) {
+            
+            // Hacemos waitpid específicamente al GRUPO DE PROCESOS de este job (-job_table[i].pgid).
+            // Usamos un bucle while porque en una pue pueden terminar varios procesos 
+            // casi simultáneamente y queremos recogerlos todos --> Ejemplo sleep 2 | sleep 10 , antes matamos el job cuando acababa el 2 pero realmanet hay que esperar al 10.
+            
+            // WNOHANG: para que waitpid no bloquee y devuelva 0 si ningún hijo ha cambiado de estado
+            // WUNTRACED: para que waitpid informe si un proceso ha sido detenido (Ctrl+Z)
+            // WCONTINUED: para que waitpid informe si un proceso ha sido reanudado (bg)
+            while ((pid = waitpid(-job_table[i].pgid, &status, WNOHANG | WUNTRACED | WCONTINUED)) > 0) {
 
-        job = find_job(pgid);
-        if(job == NULL){
-            continue;
-        }
-        //a través de status comprobamos si el proceso ha sido:
-        //caso 1: terminado con exit o con alguna otra señal
-        if(WIFEXITED(status) || WIFSIGNALED(status)){
-            job->processes_running--;
-            if(job->processes_running == 0){ //si era el último entonces pasa a DONE
-                printf("\n[%d]+ DONE\t\t%s\n", job->job_id, job->command);
-                job->status = DONE;
-                job->pgid = 0; //para liberarlo, usamos -> ya que es un puntero a job lo que tenemos
+                // CASO 1: Terminado (exit o señal)
+                if (WIFEXITED(status) || WIFSIGNALED(status)) {
+                    // En lugar de marcar DONE inmediatamente, restamos 1 al contador de procesos vivos.
+                    // Si lanzamos "ls | wc", son 2 procesos. Si acaba "ls", queda 1. El trabajo no acaba.
+                    job_table[i].pending_procs--; 
+                    // Solo si el contador llega a 0, significa que TODOS los procesos del pipe han terminado.
+                    if (job_table[i].pending_procs <= 0) {
+                        printf("\n[%d]+ Done\t\t%s\n", job_table[i].job_id, job_table[i].command);
+                        
+                        job_table[i].status = DONE;
+                        job_table[i].pgid = 0; // Liberamos el slot poniendo pgid a 0
+                    }
+                }
+                
+                // CASO 2: Detenido (STOPPED)
+                else if (WIFSTOPPED(status)) {
+                    // Si el trabajo no estaba marcado como parado, lo marcamos y avisamos al usuario
+                    if (job_table[i].status != STOPPED) {
+                        job_table[i].status = STOPPED; // cambiamos el estado del job
+                        printf("\n[%d]+ Stopped\t\t%s\n", job_table[i].job_id, job_table[i].command);
+                    }
+                }
+                
+                // CASO 3: Reanudado (CONTINUED)
+                else if (WIFCONTINUED(status)) { 
+                    // Si el trabajo estaba parado y ahora corre, actualizamos estado
+                    if (job_table[i].status != RUNNING) {
+                        job_table[i].status = RUNNING;
+                        printf("\n[%d]+ Running\t\t%s\n", job_table[i].job_id, job_table[i].command);
+                    }
+                }
             }
-        }else if(WIFSTOPPED(status)){ //caso 2: ha sido detenido
-            job->status = STOPPED; //cambiamos el estado del job
-            printf("\n[%d]+ STOPPED\t\t%s\n", job->job_id, job->command);
-        }else if(WIFCONTINUED(status)){  //caso 3: ha sido reanudado
-            job->status = RUNNING;
-            printf("\n[%d]+ Running\t\t%s\n", job->job_id, job->command);
         }
     }
 }
 
 void execute_internal_jobs(tline *line){
     int i;
-    char marker = ' '; //de momento no pongo + o - lo dejaremos para cuando tengamos el bg y tal pq es más dificil de implementar
-    if(line->ncommands > 1 || line->commands[0].argc > 1 || line->redirect_input != NULL || line->redirect_output != NULL || line->redirect_error != NULL){
-        fprintf(stderr,"Error no se puede usar jobs con pipes, redirecciones o argumentos.");
+    int current_id = -1;  // Para el signo +
+    int previous_id = -1; // Para el signo -
+
+    if(line->ncommands > 1 || line->commands[0].argc > 1 || line->redirect_input != NULL){
+        fprintf(stderr,"jobs: Error. No se permite usar jobs con argumentos o pipes\n");
         return;
     }
-    //ahora tenemos que imprimir la lista de trabajos que tenemos
+
+    // PASO 1: Identificar quién es el '+' y quién el '-'
+    // Como next_job_id siempre crece, el ID más alto es el más reciente.
     for(i = 0; i < MAX_JOBS; i++){
         if(job_table[i].pgid != 0 && job_table[i].status != DONE){
-            char *status;
-            if(job_table[i].status == RUNNING){
-                status = "Running";
-            }else if(job_table[i].status == STOPPED){
-                status = "Stopped";
+            if (job_table[i].job_id > current_id) {
+                previous_id = current_id;   // El que era rey pasa a ser príncipe
+                current_id = job_table[i].job_id; // Nuevo rey
+            } else if (job_table[i].job_id > previous_id) {
+                previous_id = job_table[i].job_id;
             }
+        }
+    }
+
+    // PASO 2: Imprimir con los marcadores
+    for(i = 0; i < MAX_JOBS; i++){
+        if(job_table[i].pgid != 0 && job_table[i].status != DONE){
+            char *status_str = (job_table[i].status == RUNNING) ? "Running" : "Stopped";
+            char marker = ' ';
             
-            //formato: [job_table[i].job_id]+ job_table[i].status job_table[i].command
-            printf("[%d]%c %s\t%s\n", job_table[i].job_id, marker, status,job_table[i].command);
+            if (job_table[i].job_id == current_id) 
+            {
+                marker = '+';
+            }
+            else if (job_table[i].job_id == previous_id) 
+            {
+                marker = '-';
+
+            }
+            printf("[%d]%c %s\t\t%s\n", job_table[i].job_id, marker, status_str, job_table[i].command);
         }
     }
 }
@@ -425,7 +506,7 @@ void register_job(pid_t pgid, const char *command, int ncommands){
             job_table[i].job_id = next_job_id++;
             job_table[i].status = RUNNING;
             job_table[i].ncommands = ncommands;
-            job_table[i].processes_running = ncommands;
+            job_table[i].pending_procs = ncommands;
             strncpy(job_table[i].command, command, MAX_LINE_BUFFER - 1); 
             job_table[i].command[MAX_LINE_BUFFER - 1] = '\0';
             printf("[%d] %d\n", job_table[i].job_id, pgid); //para informar al usuario
@@ -434,4 +515,63 @@ void register_job(pid_t pgid, const char *command, int ncommands){
     }
     //si no ha encontrado ningún hueco es que la tabla está llena así que tiene que dar un error
     fprintf(stderr, "msh: Límite de trabajos en segundo plano alcanzado.\n");
+}
+
+void execute_internal_bg(tline *line){
+    int job_id;
+    job_t *job = NULL;
+    int i;
+
+    //Comprobar si se ha llamado como bg solo -> entonces reanudamos ell ultimo paradpo
+   if (line->commands[0].argc == 1) {
+        int max_stopped_id = -1;
+        //Buscar el trabajo STOPPED con MAYOR ID
+        for (i = 0; i < MAX_JOBS; i++) {
+            if (job_table[i].pgid != 0 && job_table[i].status == STOPPED) {
+                if (job_table[i].job_id > max_stopped_id) {
+                    max_stopped_id = job_table[i].job_id;
+                    job = &job_table[i];
+                }
+            }
+        }
+        
+        if (job == NULL) {
+            fprintf(stderr, "bg: no hay trabajos detenidos actuales\n");
+            return;
+        }
+    }
+    //caso de que el bg vaya con el numero del proceso especifico
+    else
+    {
+        job_id = atoi(line->commands[0].argv[1]);
+        for (i = 0; i < MAX_JOBS ; i++)
+        {
+            if(job_table[i].job_id == job_id  && job_table[i].pgid != 0)
+            {
+                job = &job_table[i];
+                break;
+            }
+        }
+        if (job == NULL) {
+            fprintf(stderr, "bg: el trabajo %d no existe\n", job_id);
+            return;
+        }
+        if (job->status == RUNNING){
+            fprintf(stderr, "bg: el trabajo %d ya está en ejecución en segundo plano\n", job_id);
+            return;
+        }
+    }
+    // ACCIÓN: Enviar señal SIGCONT para despertar al proceso
+    // Usamos -pgid para enviar la señal a TODO el grupo de procesos 
+    if (kill(-job->pgid,SIGCONT) < 0) 
+    {
+        perror("bg: Error al enviar señal SIGCONT");
+    } else 
+    {
+        job->status = RUNNING;
+        printf("[%d]+ Running\t%s\n", job->job_id, job->command);
+    }
+
+
+
 }
